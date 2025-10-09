7bd5e0b652ebf480ec76216caaa7278093df16ea
Medasani Charan <charanchinna646@gmail.com>
2025-10-09 14:09:01 +0530

    shell-practice

--- redis.sh
+++ redis.sh
@@ -56,4 +56,4 @@ VALIDATE $? "Starting redis"
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME - $START_TIME))
 
-echo -e "script execution completed successfully, $Y time taken: $TOTAL_time $N"
+echo -e "script execution completed successfully, $Y time taken: $TOTAL_TIME $N"
